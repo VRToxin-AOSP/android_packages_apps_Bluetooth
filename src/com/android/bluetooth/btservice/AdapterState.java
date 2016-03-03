83023d82a3d82e757176fe49843b7e2d04504042
Srinu Jella <sjella@codeaurora.org>
2016-03-03 13:12:11 +0530

    Taking corrective actions in case of timeout
    Use Case: End user will see the BT reset grey out
    
    Step: SNS testing.
    
    Failure: End user wil see the BT reset.
    
    Root Cause: While moving from OFF to BLE_ON state, we are
    able to switch BLE_TURNING_ON state which starts the
    enable_timeout timer but after timing out we are stopping
    profiles other than BLE which may still be active and can
    send callbacks while Cleanup is in process or complete.
    
    Fix: Stopping GATT profiles as well in case of enable timeout
    
    Change-Id: I75d88024a60967fb516ebd0941e8e216d00c20f6

--- src/com/android/bluetooth/btservice/AdapterState.java
+++ src/com/android/bluetooth/btservice/AdapterState.java
@@ -435,6 +435,7 @@ final class AdapterState extends StateMachine {
                 case BREDR_START_TIMEOUT:
                     errorLog("Error enabling Bluetooth (start timeout)");
                     mPendingCommandState.setTurningOn(false);
+                    adapterService.stopProfileServices();
                     transitionTo(mBleOnState);
                     notifyAdapterStateChange(BluetoothAdapter.STATE_BLE_ON);
                     break;
@@ -443,6 +444,8 @@ final class AdapterState extends StateMachine {
                     errorLog("Error enabling Bluetooth (enable timeout)");
                     mPendingCommandState.setBleTurningOn(false);
                     transitionTo(mOffState);
+                    adapterService.stopProfileServices();
+                    adapterService.stopGattProfileService();
                     notifyAdapterStateChange(BluetoothAdapter.STATE_OFF);
                     break;
 
